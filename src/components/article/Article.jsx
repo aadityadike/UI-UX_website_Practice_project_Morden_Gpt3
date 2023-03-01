86ad6ceb23722774f690a5293c676b87b0c01a4c
aadityadike <adityadike123@gmail.com>
2023-03-01 16:24:12 +0530

    blog section added

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ image, date, text }) => {
+  return (
+    <div className="gpt3_article">
+      <div className="gpt3_article-image_container">
+        <img src={image} alt="image" />
+      </div>
+      <div className="gpt3_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import "./blog.css";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 
 const Blog = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="gpt3_blog section_padding">
+      <div className="gpt3_blog-heading">
+        <h1 className="gradient_text">
+          A lot is happening, <br /> We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3_blog-content_container">
+        <div className="gpt3_blog-content-box1">
+          <Article
+            image={blog01}
+            date={"20 june 2023"}
+            text={"GPT-3 and Open  AI is the future. Let us explore how it is?"}
+          />
+        </div>
+        <div className="gpt3_blog-content-box2">
+          <Article
+            image={blog02}
+            date={"2 march 2023"}
+            text={"GPT-3 and Open  AI is the future. Let us explore how it is?"}
+          />
+          <Article
+            image={blog03}
+            date={"21 may 2023"}
+            text={"GPT-3 and Open  AI is the future. Let us explore how it is?"}
+          />
+          <Article
+            image={blog04}
+            date={"13 january 2023"}
+            text={"GPT-3 and Open  AI is the future. Let us explore how it is?"}
+          />
+          <Article
+            image={blog05}
+            date={"25 september 2023"}
+            text={"GPT-3 and Open  AI is the future. Let us explore how it is?"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
